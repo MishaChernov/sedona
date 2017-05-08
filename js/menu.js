f4e5e7602f7acf907bf02c6cfb116e99a64cd448
MishaChernov <80amulet63@gmail.com>
2017-05-08 17:26:08 +0300

    Update 2017-05-08T14:25:59.762Z

--- js/menu.js
+++ js/menu.js
@@ -1,26 +1,27 @@
 'use strict';
 
 var navMenu = document.querySelector('.menu');
-var menuToggle = navMenu.querySelector('.menu__toggle');
+var burger = navMenu.querySelector('.burger');
 
 navMenu.classList.remove('menu--nojs');
 
+function toggleMenu() {
 
-function menuClosed() {
-  navMenu.classList.remove('menu--opened');
-  navMenu.classList.add('menu--closed');
-}
+  function menuClosed() {
+    navMenu.classList.remove('menu--opened');
+    navMenu.classList.add('menu--closed');
+  }
+  function menuOpened() {
+    navMenu.classList.remove('menu--closed');
+    navMenu.classList.add('menu--opened');
+  }
 
-function menuOpened() {
-  navMenu.classList.remove('menu--closed');
-  navMenu.classList.add('menu--opened');
-}
-
-menuToggle.addEventListener('click', function(event) {
-  if(navMenu.classList.contains('menu--opened')){
+  if (navMenu.classList.contains('menu--opened')) {
     menuClosed();
   }
   else {
     menuOpened();
   }
-});
+}
+
+burger.addEventListener('click', toggleMenu);
